2ac11280059e1a6003b9988787bd39274afb790e
Luis Zaninetti <35489790+luchozaninetti@users.noreply.github.com>
2024-01-17 10:24:08 +0000

    Add files via upload

---
+++ README.mk
@@ -0,0 +1,7 @@
+Processador:	Intel(R) Core(TM) i3-1005G1 CPU @ 1.20GHz, 1190 Mhz, 2 Núcleo(s), 4 Processador(es) Lógico(s)
+Data/versão: de BIOS	AMI F.17, 9/15/2021
+Memória Física (RAM) Instalada:	8.00 GB
+
+Processadores: ACPI\GenuineIntel_-_Intel64_Family_6_Model_126plax
+Placa Gráfica: PCI\VEN_8086&DEV_8A56&SUBSYS_868C103C&REV_07
+Monitor: MONITOR\CMN142C
